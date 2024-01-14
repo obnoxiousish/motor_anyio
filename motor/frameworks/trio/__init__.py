18341df3c0ddba0b46cff12217d962bf1d93a066
oewpgjewoghjewoghewg <eojweogijewogewjg@Oijgweiojwgiewjg.ewijewgi>
2024-01-14 00:10:18 -0800

    gwwegwg

--- motor/frameworks/trio/__init__.py
+++ motor/frameworks/trio/__init__.py
@@ -47,6 +47,15 @@ def pymongo_class_wrapper(f, pymongo_class):
 
     return _wrapper
 
+def check_event_loop(loop):
+    if not isinstance(loop, trio._core._run._SyncIOThread):
+        raise RuntimeError(
+            "Motor requires a Trio event loop, not %r" % loop)
+
+def get_event_loop(loop=None):
+    if loop is None:
+        loop = trio._core._run._SyncIOThread.current()
+    return loop
 
 def platform_info():
     return "trio"
--- testMotorTrio.py
+++ testMotorTrio.py
@@ -4,12 +4,14 @@ from motor.motor_trio import TrioMotorClient
 
 
 async def main():
-    client = TrioMotorClient()
+    client = TrioMotorClient("mongodb://asshole:cockButt123!@lucy.obnoxious.lol:27017")
 
-    db = client["database"]
-    collection = db["collection"]
+    db = client["scene"]
+    collection = db["testing"]
 
-    await collection.insert_one({"test": "test"})
+    #await collection.insert_one({"test": "test"})
+    count = await collection.count_documents({"test": "test"})
 
+    print(count)
 
 trio.run(main)
